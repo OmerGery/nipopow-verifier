f371149c186f1d92080b8a6c9a7ef2e36c8202f4
Omer Gery <omergery1996@gmail.com>
2022-06-10 16:51:27 +0300

    add test

---
+++ index.test.js
@@ -0,0 +1,21 @@
+
+const axios = require('axios');
+require('dotenv').config();
+jest.setTimeout(60 * 1000);
+const projectId = process.env.PROJECT_ID;
+const network = axios.create({
+    baseURL: 'https://mainnet.infura.io',
+  }); 
+describe('Test', () => {
+    it('Logger test', async() => {
+      const { data } = await network.post(`/v3/${projectId}`,{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1} );
+      console.log(data);
+      expect(data).toBeDefined();
+      expect(data.jsonrpc).toBeDefined();
+      expect(data.id).toBeDefined();
+      expect(data.result.length).toBe(8);
+
+    });
+});
+
+

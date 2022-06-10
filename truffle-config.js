fe65bb95d2a9d5c39ebfaa55a8280ec6be14e559
Omer Gery <omergery1996@gmail.com>
2022-06-10 14:12:35 +0300

    wip

---
+++ truffle-config.js
@@ -0,0 +1,28 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const fs = require("fs");
+const secrets = JSON.parse(fs.readFileSync(".secrets.json").toString().trim());
+console.log(secrets);
+module.exports = {
+    networks: {
+        kovan: {
+            networkCheckTimeout: 10000,
+            provider: () => {
+               return new HDWalletProvider(
+                 secrets.mnemonic,
+                 `wss://kovan.infura.io/ws/v3/${secrets.projectId}`
+               );
+            },
+            network_id: "42",
+         },         
+      development: {
+        host: "127.0.0.1",
+        port: 8545,
+        network_id: "*" // Match any network id
+      }
+    },
+    compilers: {
+      solc: {
+        version: "^0.6.0"
+      }
+    }
+  };
